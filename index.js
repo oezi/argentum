593de1ceed676219b76f1b4dbf212c26a6efe2bf
Steynamit3 <raiko.stein@gmail.com>
2018-04-16 14:58:45 +0200

    Neue Module

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const helmet = require('helmet');
 const bodyparser = require('body-parser');
 const pgpromise = require('pg-promise');
 const mssql = require('mssql');
+const session = require('express-session');
+const csrf = require('csurf');
+const cors = require('cors');
+const PgSession = require('connect-pg-simple')(session);
 
 const logger = require('./modules/logger');
 
@@ -14,5 +18,9 @@ module.exports = {
   bodyparser,
   pgpromise,
   mssql,
-  logger
+  logger,
+  session,
+  csrf,
+  cors,
+  PgSession
 };
